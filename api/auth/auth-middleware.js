8c64233b2fecd5cb2f53b2385bb24cd43a014beb
priscila monteiro <pririmonteiro@me.com>
2021-12-17 09:54:45 -0800

    ran prettier

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,51 +1,48 @@
-const User = require('../users/users-model');
-const { findBy } = require('../users/users-model');
+const User = require("../users/users-model");
+const { findBy } = require("../users/users-model");
 
 function validUserBody(req, res, next) {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   if (!username || !password) {
-   res.status(422).json({ message: 'username and password required' })
-    } else {
-        next()
-    }
+    res.status(422).json({ message: "username and password required" });
+  } else {
+    next();
+  }
 }
 
-
-async function checkUsernameExists (req, res, next) {
-  
+async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username });
-    if (!users.length) { 
+    if (!users.length) {
       next();
-    }
-    else {
+    } else {
       next({ status: 422, message: "username taken" });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function validatedUser(req, res, next) {
-    try {
-        const { username } = req.body;
-        const user = await findBy({ username: username });
-        if (user.length) {
-            req.user = user[0];
-            next();
-        } else {
-            next({
-                status: 401,
-                message: "invalid credentials"
-            });
-        }
-    } catch (err) {
-        next(err);
+  try {
+    const { username } = req.body;
+    const user = await findBy({ username: username });
+    if (user.length) {
+      req.user = user[0];
+      next();
+    } else {
+      next({
+        status: 401,
+        message: "invalid credentials",
+      });
     }
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = {
   validUserBody,
   checkUsernameExists,
   validatedUser,
-}
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,49 +1,47 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const tokenBuilder = require('./token-builder');
-const User = require('../users/users-model');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const tokenBuilder = require("./token-builder");
+const User = require("../users/users-model");
 
-
-const { 
+const {
   validUserBody,
   checkUsernameExists,
   validatedUser,
-} = require('./auth-middleware')
+} = require("./auth-middleware");
 
-router.post('/register',validUserBody, checkUsernameExists, (req, res, next) => {
-  const { username, password } = req.body
-  const rounds = process.env.BCRYPT_ROUNDS || 8
-  const hash = bcrypt.hashSync(password, rounds)
-  const newUser = {
-    username: username,
-    password: hash,
+router.post(
+  "/register",
+  validUserBody,
+  checkUsernameExists,
+  (req, res, next) => {
+    const { username, password } = req.body;
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcrypt.hashSync(password, rounds);
+    const newUser = {
+      username: username,
+      password: hash,
+    };
+
+    User.add(newUser)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch(next);
   }
+);
 
-  User.add(newUser)
-    .then((user) => {
-      res.status(201).json(user)
-    })
-    .catch(next); 
-});
-
-
-router.post('/login', validUserBody, validatedUser, (req, res, next) => {
+router.post("/login", validUserBody, validatedUser, (req, res, next) => {
   let { password } = req.body;
-    if ( bcrypt.compareSync(password, req.user.password)) {
-      const token = tokenBuilder(req.user) 
-      res.status(200).json({
-        message: `welcome, ${req.user.username}`, token
-      });
-    } else {
-      next(
-        { status: 401, message: 'Invalid credentials' }
-      )
-    }
+  if (bcrypt.compareSync(password, req.user.password)) {
+    const token = tokenBuilder(req.user);
+    res.status(200).json({
+      message: `welcome, ${req.user.username}`,
+      token,
+    });
+  } else {
+    next({ status: 401, message: "Invalid credentials" });
+  }
 });
-
-
-
-
 
 //     1- In order to log into an existing account the client must provide `username` and `password`:
 //       {
